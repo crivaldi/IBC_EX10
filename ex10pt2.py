c31cfca2214562488253cf2b68b186ed9694138a
Patricia Portmann <pportman@nd.edu>
2018-11-15 18:00:02 -0500

    Just started pt 2

---
+++ ex10pt2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 15 17:57:19 2018
+
+Exercise10 part 2: Use Lotka and Volterra competition model to demonstrate
+the validity of alpha12 < alpha21 and alpha21 < alpha22 in three different 
+model simulations
+
+@author: Patricia
+"""
+
